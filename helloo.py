0a994bdfb022715b75aee2ebacc35208adef7e6c
shrenika_koduru <9866shrenika@gmail.com>
2025-07-09 10:23:59 +0530

    second commit

--- helloo.py
+++ helloo.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def helloo():
-    return("Hello World, this is shreya ")
+    return("Hello World, this is shreya reddy ")
 if __name__ == "__main__":
     app.run(debug=True)
