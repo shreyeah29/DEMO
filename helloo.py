541d1a0117f76b91cce0eb17f8087a65e2755d64
shreyeah29 <shreyareddys29@gmail.com>
2025-07-09 10:43:24 +0530

    my project

--- helloo.py
+++ helloo.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def helloo():
-    return("Hello World, this is shreya reddy ")
+    return("Hello World, this is shreya reddys ")
 if __name__ == "__main__":
     app.run(debug=True)
